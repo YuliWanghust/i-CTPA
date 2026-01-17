6ac5d9732083771feef7b5e5e291dfefc1858322
YuliWanghust <44452178+YuliWanghust@users.noreply.github.com>
2026-01-16 19:35:49 -0500

    Modify paths in eval.sh for new dataset
    Updated paths for pretrained model, mimicit, and images.

--- eval.sh
+++ eval.sh
@@ -3,9 +3,9 @@ export PYTHONPATH="your PYTHON PATH"
 #This code capture the report output during the optimization process and record the report to an excel file (which should be specified in /pipeline/train/eval.py
 CUDA_VISIBLE_DEVICES=1 accelerate launch --config_file=./evaluation/pipeline/accelerate_configs/accelerate_config_fsdp.yaml \
 ./evaluation/pipeline/train/eval.py \
---pretrained_model_name_or_path="./checkpoints/OTTER_CLIP_RADICAL_hf/" \
---mimicit_path="./data/CQ500p_instruction.json" \
---images_path="./data/CQ500p.json" \
+--pretrained_model_name_or_path="./checkpoints/OTTER_CLIP_xxxx_xx/" \
+--mimicit_path="./data/xxxx_instruction.json" \
+--images_path="./data/xxxx.json" \
 --batch_size=1 \
 --warmup_steps_ratio=0.01 \
 --workers=1 \
